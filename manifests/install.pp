e41ff29467dba61ada1f314ab4a7b36ff44fe52f
msfraser <msfraser@gmail.com>
2019-03-06 15:07:48 +1000

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,54 +9,85 @@ class lightdm::install (
 
   if $make_default {
 
-    file { 'default-display-manager':
-      ensure  => 'file',
-      path    => '/etc/X11/default-display-manager',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => "/usr/sbin/lightdm\n"
-    }
+    case $::osfamily {
+      'Debian': {
 
-    # Stop current display-manager before we install lightdm
-    exec { 'stop other display-manager':
-      command => 'systemctl stop display-manager',
-      onlyif  => 'test "$(readlink /etc/systemd/system/display-manager.service)" != "/lib/systemd/system/lightdm.service"',
-      path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-      user    => 'root',
-      before  => Package['lightdm'],
-      notify  => Exec['set shared/default-x-display-manager']
-    }
+        file { 'default-display-manager':
+          ensure  => 'file',
+          path    => '/etc/X11/default-display-manager',
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => "/usr/sbin/lightdm\n"
+        }
 
-    # Set prefered display-manager to lightdm
-    exec { 'set shared/default-x-display-manager':
-      command     => 'echo lightdm shared/default-x-display-manager select lightdm | debconf-set-selections',
-      path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-      user        => 'root',
-      refreshonly => true,
-      notify      => Exec['dpkg-reconfigure lightdm'],
-      require     => Package['lightdm']
-    }
+        # Stop current display-manager before we install lightdm
+        exec { 'stop other display-manager':
+          command => 'systemctl stop display-manager',
+          onlyif  => 'test "$(readlink /etc/systemd/system/display-manager.service)" != "/lib/systemd/system/lightdm.service"',
+          path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+          user    => 'root',
+          before  => Package['lightdm'],
+          notify  => Exec['set shared/default-x-display-manager']
+        }
 
-    # We need to dpkg-reconfigure lightdm after setting default-x-display-manager
-    # to rebuild the display-manager.service symlink for systemd
-    exec { 'dpkg-reconfigure lightdm':
-      command     => 'dpkg-reconfigure lightdm',
-      path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-      user        => 'root',
-      refreshonly => true,
-      notify      => Class['lightdm::service'],
-      require     => [Exec['set shared/default-x-display-manager'], File['default-display-manager']]
-    }
+        # Set prefered display-manager to lightdm
+        exec { 'set shared/default-x-display-manager':
+          command     => 'echo lightdm shared/default-x-display-manager select lightdm | debconf-set-selections',
+          path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+          user        => 'root',
+          refreshonly => true,
+          notify      => Exec['dpkg-reconfigure lightdm'],
+          require     => Package['lightdm']
+        }
 
-    package { 'lightdm':
-      name     => $package_name,
-      ensure   => $package_ensure,
-      provider => $package_provider,
-      before   => File['default-display-manager'],
-      notify   => Exec['set shared/default-x-display-manager']
-    }
+        # We need to dpkg-reconfigure lightdm after setting default-x-display-manager
+        # to rebuild the display-manager.service symlink for systemd
+        exec { 'dpkg-reconfigure lightdm':
+          command     => 'dpkg-reconfigure lightdm',
+          path        => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+          user        => 'root',
+          refreshonly => true,
+          notify      => Class['lightdm::service'],
+          require     => [Exec['set shared/default-x-display-manager'], File['default-display-manager']]
+        }
 
+        package { 'lightdm':
+          name     => $package_name,
+          ensure   => $package_ensure,
+          provider => $package_provider,
+          before   => File['default-display-manager'],
+          notify   => Exec['set shared/default-x-display-manager']
+        }
+      }
+      
+      'RedHat': {
+        file { 'default-display-manager':
+          ensure  => 'file',
+          path    => '/etc/sysconfig/desktop',
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => "DISPLAYMANAGER=lightdm\n"
+        }
+        
+        # Stop current display-manager before we install lightdm
+        exec { 'stop other display-manager':
+          command => 'systemctl stop display-manager',
+          onlyif  => 'test "$(readlink /etc/systemd/system/display-manager.service)" != "/usr/lib/systemd/system/lightdm.service"',
+          path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+          user    => 'root',
+          before  => Package['lightdm'],
+        }
+        
+        package { 'lightdm':
+          name     => $package_name,
+          ensure   => $package_ensure,
+          provider => $package_provider,
+          before   => File['default-display-manager'],
+        }
+      }
+    }
   } else {
 
     package { 'lightdm':
